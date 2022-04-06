15929e0b83f8322dbc226862ecbcecf24eb2f562
Vitor Hugo <vitorhprsilva@gmail.com>
2022-04-06 00:10:45 -0300

    feat: HomeScreen create

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Logo from '../../assets/PokemonLogo.png'
+
+import './styles.css'
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/map">Map</Link>
+    <div className='initial-home-page'>
+      <img src={Logo} alt='logo pokemon' />
+      <Link to="/map" className='btn-start'>START</Link>
     </div>
   )
 }
