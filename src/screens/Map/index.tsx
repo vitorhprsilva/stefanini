d009d81d51c3604256e0c4fde5e10679b7b0ec74
Vitor Hugo <vitorhprsilva@gmail.com>
2022-04-06 12:13:34 -0300

    feat: increment slots to map screen

--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { SlotEmpty } from '../../components/slot-empty'
+
+import IconAnswer from '../../assets/IconAnswer.svg'
+import ImageBulbasaur from '../../assets/imageBulbasaur.png'
+
+import { Pokemon, defaultPokemon } from '../../domain'
+
+import './styles.css'
 
 export const MapPokemon = () => {
+
+  
+  let listPokemons: Pokemon[] = [defaultPokemon, defaultPokemon, defaultPokemon, defaultPokemon, defaultPokemon, defaultPokemon]
+  const [pokemons, setPokemons] = useState<Pokemon[]>();
+  
+
   return (
-    <div>
-      <h1>Map</h1>
-      <Link to="/">Home</Link>
+    <div className='map-pokemon'>
+      <div className='slots-pokemon'>
+        {
+          listPokemons.map((pokemon) => {
+            return (
+              <div key={pokemon.name}>
+                <SlotEmpty color='#00D68F' borderColor='#004A45'>
+                  {pokemon.name==='' ? <img src={IconAnswer} alt='icon answer' /> : <img src={ImageBulbasaur} alt='pokemon bulbasaur' />}
+                </SlotEmpty>
+              </div>
+            )
+          })
+        }
+        
+      </div>
     </div>
   )
 }
